54ffa1d523473aeb7dd2787fe8e6277d6dd723d3
sumukshashidhar <sumukuuu@gmail.com>
2020-11-14 00:52:53 +0530

    huge progress

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,6 +1,7 @@
 # IMPORTS
-from flask import Flask
+from flask import Flask, request
 
 # Initializing the Flask app
 app = Flask(__name__)
 
+import auth
--- src/flask/auth.py
+++ src/flask/auth.py
@@ -1,2 +1,23 @@
+from flask import Flask, request
 from app import app
+import sqlite3
 
+def hash_password(passw):
+    return passw
+
+
+
+@app.route('/createUser', methods=['POST'])
+def create_user():
+    uname = request.form['username']
+    passw = request.form['password']
+    passw = hash_password(passw)
+    try:
+        conn = sqlite3.connect('./../../db/dbase.db')
+        CREATE_STRING = f'INSERT INTO users (userid, username, password) VALUES (1, "{uname}", "{passw}")'
+        conn.execute(CREATE_STRING)
+        conn.commit()
+        conn.close()
+    except:
+        return 500
+    return 200
