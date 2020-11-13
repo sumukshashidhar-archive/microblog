79096d75dda2ed4e1af00db2b6e4bd12b725b120
sumukshashidhar <sumukuuu@gmail.com>
2020-11-13 23:57:37 +0530

    2020-11-13 23:57:37.045589

--- src/file_changes_scripts/detect_changes.py
+++ src/file_changes_scripts/detect_changes.py
@@ -1,9 +1,18 @@
-import os
 import subprocess
+import os
+import datetime
+import time
+
+
+def git_add_and_commit():
+    os.system(f'(git add . && git commit -m "{datetime.datetime.now()}" && git push) &')
+
 
 if __name__ == "__main__":
-    output = subprocess.run(['git', 'status'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    if "nothing to commit, working tree clean" in output:
-        print("Clean")
-    else:
-        print("Dirty")
+    while True:
+        output = subprocess.run(['git', 'status'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        if "nothing to commit, working tree clean" in output:
+            print("Clean")
+        else:
+            git_add_and_commit()
+        time.sleep(120)
